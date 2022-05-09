4a724a4799cd536cd9e79abbcb729a444f80b66f
Shivam Jindal <iamshivam007>
2022-05-09 12:04:14 +0530

    Add API exception handler

--- src/main/java/io/dropwizard/resources/SayingResource.java
+++ src/main/java/io/dropwizard/resources/SayingResource.java
@@ -3,15 +3,13 @@ package io.dropwizard.resources;
 import com.codahale.metrics.annotation.Timed;
 import io.dropwizard.api.Saying;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
-@Path("/hello-world")
+@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 public class SayingResource {
 
@@ -26,9 +24,22 @@ public class SayingResource {
     }
 
     @GET
+    @Path("hello-world")
     @Timed
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
         final String value = String.format(template, name.orElse(defaultName));
         return new Saying(counter.incrementAndGet(), value);
+    }
+
+    @GET
+    @Path("error")
+    public String testException() {
+        throw new WebApplicationException("Manually raised error!!");
+    }
+
+    @GET
+    @Path("error404")
+    public String testException404() {
+        throw new WebApplicationException("Manually raised error!!", Response.Status.NOT_FOUND);
     }
 }
